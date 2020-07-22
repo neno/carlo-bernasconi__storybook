855d9706478191fd5be16e6d61e2d4635f1f022b
Nenad Brcic <neno@neno.ch>
2020-07-22 10:03:50 +0200

    Add colors

---
+++ src/01-atoms/colors/colors.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import colors from './colors.scss';
+
+export default {
+  title: '01-Atoms/Colors',
+};
+
+const ColorBox = ({ color }) => {
+  const styles = {
+    display: 'block',
+    backgroundColor: color,
+    width: '100px',
+    height: '100px',
+  };
+  return (
+    <div>
+      <div style={styles}></div>
+      <p>{color}</p>
+    </div>
+  );
+};
+
+export const dark = () =>
+  renderToStaticMarkup(<ColorBox color={colors.colorDark} />);
+
+export const medium = () =>
+  renderToStaticMarkup(<ColorBox color={colors.colorMedium} />);
+
+export const light = () =>
+  renderToStaticMarkup(<ColorBox color={colors.colorLight} />);
