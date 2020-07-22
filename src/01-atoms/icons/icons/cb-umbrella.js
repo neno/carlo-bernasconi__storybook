d35c2dde0b49aa837fd86a66bd71bc28f9c1888d
Nenad Brcic <neno@neno.ch>
2020-07-22 10:02:27 +0200

    Add icons

---
+++ src/01-atoms/icons/icons/cb-umbrella.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const CBUmbrella = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+    width='65'
+    height='53'
+    viewBox='0 0 65 53'
+  >
+    <defs>
+      <clipPath id='clip-carlo_umbrella'>
+        <rect width='65' height='53' />
+      </clipPath>
+    </defs>
+    <g id='carlo_umbrella' clip-path='url(#clip-carlo_umbrella)'>
+      <rect width='65' height='53' fill='rgba(255,255,255,0)' />
+      <g id='Gruppe_4' data-name='Gruppe 4' transform='translate(-133 -2345.5)'>
+        <path
+          id='hexagon'
+          d='M38.8,29.844V13.932a3.978,3.978,0,0,0-1.989-3.441L22.89,2.535a3.978,3.978,0,0,0-3.978,0L4.989,10.491A3.978,3.978,0,0,0,3,13.932V29.844a3.978,3.978,0,0,0,1.989,3.441L18.912,41.24a3.978,3.978,0,0,0,3.978,0l13.923-7.956A3.978,3.978,0,0,0,38.8,29.844Z'
+          transform='translate(130 2355.786)'
+          fill='#f4f4f4'
+        />
+        <path
+          id='umbrella'
+          d='M52.7,25.5A25.967,25.967,0,0,0,1,25.5ZM40.95,41.95a7.05,7.05,0,0,1-14.1,0V25.5'
+          transform='translate(141.5 2346.5)'
+          fill='none'
+          stroke='#c8652b'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          stroke-width='6'
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default CBUmbrella;
---
+++ src/01-atoms/icons/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import ArrowLeft from './icons/arrow-left';
+import ArrowRight from './icons/arrow-right';
+import ChevronDown from './icons/chevron-down';
+import Facebook from './icons/facebook';
+import Home from './icons/home';
+import Instagram from './icons/instagram';
+import Mail from './icons/mail';
+import MapPin from './icons/map-pin';
+import Minus from './icons/minus';
+import Phone from './icons/phone';
+import Plus from './icons/plus';
+import Search from './icons/search';
+import ShoppingCart from './icons/shopping-cart';
+import Trash from './icons/trash';
+import Twitter from './icons/twitter';
+import Youtube from './icons/youtube';
+import CBDesignElement from './icons/cb-design-element';
+import CBHeart from './icons/cb-heart';
+import CBMapPin from './icons/cb-map-pin';
+import CBTool from './icons/cb-tool';
+import CBUmbrella from './icons/cb-umbrella';
+
+const icons = {
+  'arrow-left': ArrowLeft,
+  'arrow-right': ArrowRight,
+  'chevron-down': ChevronDown,
+  facebook: Facebook,
+  home: Home,
+  instagram: Instagram,
+  mail: Mail,
+  'map-pin': MapPin,
+  minus: Minus,
+  phone: Phone,
+  plus: Plus,
+  search: Search,
+  'shopping-cart': ShoppingCart,
+  trash: Trash,
+  twitter: Twitter,
+  youtube: Youtube,
+  'cb-design-element': CBDesignElement,
+  'cb-heart': CBHeart,
+  'cb-map-pin': CBMapPin,
+  'cb-tool': CBTool,
+  'cb-umbrella': CBUmbrella,
+};
+
+const Icon = ({ name }) => {
+  const Ico = icons[name];
+  return <Ico />;
+};
+
+export default Icon;
