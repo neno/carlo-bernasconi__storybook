960ebd3091f69a990eb3df450cebfa5e33876110
Nenad Brcic <neno@neno.ch>
2020-08-03 14:48:10 +0200

    Extract Layout

---
+++ src/04-templates/layout/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../../03-organisms/header"
+import Footer from "../../03-organisms/footer"
+
+const Layout = ({ classes, children }) => (
+  <div className={`t-template ${classes.join(" ")}`}>
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </div>
+)
+
+export default Layout
--- src/04-templates/overview-template/index.js
+++ src/04-templates/overview-template/index.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import Header from '../../03-organisms/header';
-import Footer from '../../03-organisms/footer';
-import Hero from '../../02-molecules/hero/';
-import { heroData } from '../../02-molecules/hero/stories/hero.data';
-import { pictureData } from '../../01-atoms/picture/stories/picture.data';
-import OverviewTeasers from '../../03-organisms/overview-teasers/index';
-import { teaserData } from '../../03-organisms/overview-teasers/stories/overview-teasers.data';
-import RichText from '../../01-atoms/rich-text';
+import React from "react"
+import Header from "../../03-organisms/header"
+import Footer from "../../03-organisms/footer"
+import Hero from "../../02-molecules/hero/"
+import { heroData } from "../../02-molecules/hero/stories/hero.data"
+import { pictureData } from "../../01-atoms/picture/stories/picture.data"
+import OverviewTeasers from "../../03-organisms/overview-teasers/index"
+import { teaserData } from "../../03-organisms/overview-teasers/stories/overview-teasers.data"
+import RichText from "../../01-atoms/rich-text"
+import Layout from "../layout"
 
 const OverviewTemplate = () => (
-  <div className={`t-template t-template--overview`}>
-    <Header />
+  <Layout classes={["t-template--overview"]}>
     <Hero
       data={pictureData}
       title={heroData.title}
       text={heroData.text}
-      breadcrumbItems={heroData.breadcrumbItems} />
+      breadcrumbItems={heroData.breadcrumbItems}
+    />
     <div className="l-constrainer">
       <RichText>
         <h1>Unsere Kompetenzen</h1>
-        <p>Mit unseren Steinen lassen sich alle Bereiche verschönern. ...Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <p>
+          Mit unseren Steinen lassen sich alle Bereiche verschönern. ...Lorem
+          ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+          diam voluptua.
+        </p>
       </RichText>
     </div>
     <div className="l-constrainer">
       <OverviewTeasers teasers={teaserData} />
     </div>
-    <Footer />
-  </div>
-);
+  </Layout>
+)
 
-export default OverviewTemplate;
+export default OverviewTemplate
