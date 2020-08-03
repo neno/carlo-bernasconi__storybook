83725351c215dda6695cf53fdb8afa417c58e21c
Nenad Brcic <neno@neno.ch>
2020-08-03 09:32:32 +0200

    Fix img sizes

--- src/03-organisms/overview-teasers/stories/overview-teasers.data.js
+++ src/03-organisms/overview-teasers/stories/overview-teasers.data.js
@@ -2,145 +2,181 @@ export const teaserData = [
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Küchenabdeckungen'
+    title: "Küchenabdeckungen"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Bodenbeläge und Treppen (Innen)'
+    title: "Bodenbeläge und Treppen (Innen)"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Bodenbeläge und Treppen (Aussen)'
+    title: "Bodenbeläge und Treppen (Aussen)"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Elementbau (Innen)'
+    title: "Elementbau (Innen)"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Elementbau (Aussen)'
+    title: "Elementbau (Aussen)"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Fassaden'
+    title: "Fassaden"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Gärten'
+    title: "Gärten"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Grabsteine'
+    title: "Grabsteine"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Berner Sandstein'
+    title: "Berner Sandstein"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Oberflächenbearbeitung'
+    title: "Oberflächenbearbeitung"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Badezimmer'
+    title: "Badezimmer"
   },
   {
     img: {
       dataSrcset: `
-      https://placehold.it/358x260 1@,
-      https://placehold.it/716x520 2@,
-      https://placehold.it/1074x780 3@,
+      https://placehold.it/358x260 358w,
+      https://placehold.it/716x520 716w,
+      https://placehold.it/1074x780 1074w,
+      https://placehold.it/1432x1040 1432w,
+      https://placehold.it/1790x1300 1790w,
     `,
-      dataSrc: 'https://placehold.it/358x260',
-      src: 'https://placehold.it/358x260',
+      dataSrc: "https://placehold.it/358x260",
+      src: "https://placehold.it/358x260",
+      sizes: "(min-width: 960px) 30vw, 100vw"
     },
-    title: 'Wasseraufbereitung & Abwassertechnik'
+    title: "Wasseraufbereitung & Abwassertechnik"
   }
-];
+]
