a52abf8aa54f7c6d1140158df876a87f8e841be7
Nenad Brcic <neno@neno.ch>
2020-08-03 08:32:34 +0200

    Fix overview teasers

--- src/03-organisms/overview-teasers/stories/overview-teasers.data.js
+++ src/03-organisms/overview-teasers/stories/overview-teasers.data.js
@@ -69,6 +69,18 @@ export const teaserData = [
       dataSrc: 'https://placehold.it/358x260',
       src: 'https://placehold.it/358x260',
     },
+    title: 'Fassaden'
+  },
+  {
+    img: {
+      dataSrcset: `
+      https://placehold.it/358x260 1@,
+      https://placehold.it/716x520 2@,
+      https://placehold.it/1074x780 3@,
+    `,
+      dataSrc: 'https://placehold.it/358x260',
+      src: 'https://placehold.it/358x260',
+    },
     title: 'Gärten'
   },
   {
